d513077c4504f6211149e6126c88d3dede4a22aa
malwozniak <mgrztwnk5@gmail.com>
2022-12-13 09:58:22 +0100

    1 file modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule,
-  MatToolbarModule,
-  MatIconModule],
-exports:[
-  MatIconModule
-],
+  
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [MatIconModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
